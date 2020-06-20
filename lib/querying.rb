2a2f414390d82ad8e082f1f101efc545134f2784
Rwanda S <rwanda.ace@gmail.com>
2020-06-20 19:27:09 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -12,7 +12,7 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
-  "SELECT authors.name FROM authors JOIN series ON authors.id = series. "
+  "SELECT authors.name, series.title FROM authors JOIN series ON authors.id = series.author_id; "
 end
 
 def select_series_title_with_most_human_characters
