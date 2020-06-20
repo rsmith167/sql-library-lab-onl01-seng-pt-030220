ede5ff8a07d8b43079e8150c5e69c96fb376064b
Rwanda S <rwanda.ace@gmail.com>
2020-06-20 19:14:56 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,5 +1,5 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "SELECT books.title, books.year FROM books GROUP BY books.year GROUP BY books.series_id LIMIT 1;"
+  "SELECT books.title, books.year, DISTINCT books.series_id FROM books GROUP BY books.year ;"
 end
 
 def select_name_and_motto_of_char_with_longest_motto
