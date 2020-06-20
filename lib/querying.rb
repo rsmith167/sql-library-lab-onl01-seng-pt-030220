78f1e37ed2e8e47500754dfc2d6fb0dcc99b44d2
Rwanda S <rwanda.ace@gmail.com>
2020-06-20 19:36:42 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -16,9 +16,9 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT series.title FROM series JOIN character_books ON "
+  ";"
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "Write your SQL query here"
+  "SELECT characters.name, COUNT(character_books.character_id) FROM characters JOIN character_books ON characters.id = character_books.character_id GROUP BY COUNT(character_books.character_id) DESC, characters.name ASC;"
 end
